31561aea07d7ab7a41bfda0ef5fb7fbf6ec4a12e
pshappycle <59588008+pshappycle@users.noreply.github.com>
2020-06-16 21:00:06 -0400

    final changes

--- Static/js/logic.js
+++ Static/js/logic.js
@@ -2,9 +2,9 @@
 var myMap = L.map("map", {
     center: [43.1009, -75.85],
     zoom: 6.25
-  
+
   });
-  
+
   // Adding tile layer
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -14,26 +14,26 @@ var myMap = L.map("map", {
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   }).addTo(myMap);
-  
+
   // Load in geojson data
   var geoData = "Static/data/nate.geojson";
-  
-  
+
+
   // Grab data with d3
   d3.json(geoData, function(data) {
-  
+
     // Create a new choropleth layer
     var geojson = L.choropleth(data, {
-  
+
       // Define what  property in the features to use
       valueProperty: "percentage",
-  
+
       // Set color scale (in hex values)
       scale: ["#33FFF7", "#3D33FF"],
-  
+
       // Number of breaks in step range
       steps: 10,
-  
+
       // q for quantile, e for equidistant, k for k-means
       mode: "q",
       style: {
@@ -42,21 +42,25 @@ var myMap = L.map("map", {
         weight: 1,
         fillOpacity: 0.8
       },
-  
+
       // Binding a pop-up to each layer
       onEachFeature: function(feature, layer) {
         layer.bindPopup("County: " + feature.properties.name +
                         "<br>Obesity Rate:<br>" +
-                        feature.properties.percentage +"%");
+                        feature.properties.percentage +"%")
+            .on('click', function(d){
+                console.log(feature.properties.name);
+              return d;
+            });
       }
     });
-  
+
     // Add the choropleth layer to the map
     geojson.addTo(myMap);
-  
+
     // Set up the legend
     var legend = L.control({ position: "bottomleft" });
-  
+
     // onAdd: "the map calls the onAdd() method of the layer, then the layer
     //        creates its HTML element(s) (commonly named ‘container’ element)
     //        and adds them to the map pane."
@@ -66,26 +70,25 @@ var myMap = L.map("map", {
       var limits = geojson.options.limits;
       var colors = geojson.options.colors;
       var labels = [];
-  
+
       // Add min & max
       var legendInfo = "<h1>Obesity %: </h1>" +
         "<div class=\"labels\">" +
           "<div class=\"min\">" + limits[0] + "</div>" +
           "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
         "</div>";
-  
+
       div.innerHTML = legendInfo;
-  
+
       limits.forEach(function(limit, index) {
         labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
       });
-  
+
       div.innerHTML += "<ul>" + labels.join("") + "</ul>";
       return div;
     };
-  
+
     // Adding legend to the map
     legend.addTo(myMap);
-  
+
   });
-  
